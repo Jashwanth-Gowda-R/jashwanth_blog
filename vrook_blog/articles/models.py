69a44dc2060685ba5a5bb06978aa90942612fd7d
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-02-14 00:19:50 +0530

    added admin side,added template tags,added method models

---
+++ vrook_blog/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+# run below commands after writing model code
+# python manage.py makemigrations
+# python manage.py migrate
+
+class Articles(models.Model):
+    title=models.CharField(max_length=250)
+    slug=models.SlugField()
+    body=models.TextField()
+    date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50]+ ' .....'
+
