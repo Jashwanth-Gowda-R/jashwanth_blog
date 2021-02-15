0d4b0621639770c86127ae2cb4c86f5d46aa54d1
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-02-15 16:01:15 +0530

    added url parameters for slug

--- vrook_blog/articles/urls.py
+++ vrook_blog/articles/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.articles),
+    path('<slug:slug>/', views.article_detail)
 ]
+# This has been significantly changed in Django 3, this could be done like this:
+# path('<slug:slug>/', views.article_detail),
+# where the first slug "<slug" is the type of the passed parameter which is a slug in our case, and the second slug ":slug>" is the name of the parameter which could be any name; "abc", "the_slug", etc.
+# If you still insist to use regex then there is one thing to change; the method name instead of "url" use "re_path":
+# re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
--- vrook_blog/articles/views.py
+++ vrook_blog/articles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Articles
-
+from django.http import HttpResponse
 # Create your views here.
 def articles(request):
     articles=Articles.objects.all().order_by('date')
     return render(request, 'articles/articles.html',{'articles':articles})
+
+def article_detail(request,slug):
+    return HttpResponse(slug)
