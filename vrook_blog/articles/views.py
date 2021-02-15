de30f7e90d4cba0541ef5b98965c5e30482911a2
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-02-15 19:09:15 +0530

    Uploading Media

--- vrook_blog/articles/views.py
+++ vrook_blog/articles/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Articles
 from django.http import HttpResponse
+
+
 # Create your views here.
 def articles(request):
-    articles=Articles.objects.all().order_by('date')
-    return render(request, 'articles/articles.html',{'articles':articles})
+    articles = Articles.objects.all().order_by('date')
+    return render(request, 'articles/articles.html', {'articles': articles})
 
-def article_detail(request,slug):
-    return HttpResponse(slug)
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Articles.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
--- vrook_blog/vrook_blog/urls.py
+++ vrook_blog/vrook_blog/urls.py
@@ -14,17 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views, settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/',include('articles.urls')),
-    path('about/',views.about ),
-    path('',views.homepage ),
+    path('articles/', include('articles.urls')),
+    path('about/', views.about),
+    path('', views.homepage),
 ]
 
 # urlpatterns+=staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
