e4460a90221955d3fae4c12d7eecb353b664d953
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-02-22 21:00:25 +0530

    logging user in

--- vrook_blog/accounts/views.py
+++ vrook_blog/accounts/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-
+from django.contrib.auth import login
 
 # Create your views here.
-def signup(request):
+def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # login the user
+            user=form.save()
+            login(request,user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 
-def login(request):
+def login_view(request):
     if request.method=='POST':
         form=AuthenticationForm(data=request.POST)
         if form.is_valid():
+            # login the user
+            user=form.get_user()
+            login(request,user)
             return redirect('articles:list')
     else:
         form=AuthenticationForm()
