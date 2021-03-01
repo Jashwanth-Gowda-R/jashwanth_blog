ea19173cf432de4b9fcae0e073bb2f1be48ece8d
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-03-01 19:02:06 +0530

    redirecting to homepage

--- vrook_blog/vrook_blog/urls.py
+++ vrook_blog/vrook_blog/urls.py
@@ -13,19 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
-from . import views, settings
+# from . import views, settings
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.conf.urls.static import static
+# from django.conf import settings
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
-    path('', views.homepage,name='home'),
+    path('',  article_views.articles, name="home"),
 ]
 
 # urlpatterns+=staticfiles_urlpatterns()
