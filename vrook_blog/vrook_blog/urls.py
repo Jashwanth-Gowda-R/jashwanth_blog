229dc3f69a543c01227e5847db26abb5f52aac6b
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-03-13 19:51:15 +0530

    User Profile and Picture added automatically when registered

--- vrook_blog/vrook_blog/urls.py
+++ vrook_blog/vrook_blog/urls.py
@@ -44,5 +44,6 @@ urlpatterns = [
 # urlpatterns+=staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
